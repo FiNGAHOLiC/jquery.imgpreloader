97a2a25ad569630f2eba57ec510bc89f5852829a
FiNGAHOLiC <pl4y4h4ter@gmail.com>
2013-01-31 16:14:54 +0900

    update version

--- js/jquery.imgpreloader.js
+++ js/jquery.imgpreloader.js
@@ -1,9 +1,9 @@
 /*!
  * jquery.imgpreloader.js
  *
- * @modified  2012/01/25
+ * @modified  2012/01/31
  * @requires  jQuery 1.7.x or later
- * @version   1.1.1
+ * @version   1.1.2
  * @author    FiNGAHOLiC
  * @link      https://github.com/FiNGAHOLiC/jquery.imgpreloader
  * @license   The MIT License
